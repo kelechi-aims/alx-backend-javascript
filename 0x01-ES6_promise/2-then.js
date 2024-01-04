b8c23ef76f702f0bbd07d03c96ff8dee47cd3041
kelechi-aims <kelechi@aims.edu.gh>
2024-01-04 10:23:26 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  // Append three handlers to the promise
-  promise
-    .then(() => {
-      // When the Promise resolves, return an object with the specified attributes
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch((error) => {
-      // When the Promise rejects, return an empty Error object
-      console.error(error.message); // Log the error message
-      return new Error();
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' })) // Resolve with an object on success
+    .catch(() => new Error()) // Reject with an empty Error object on failure
+    .finally(() => console.log('Got a response from the API')); // Log regardless of resolution or rejection
 }
