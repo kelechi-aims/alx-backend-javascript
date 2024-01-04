d493217f0b196add166260fe4d68bfa929117e17
kelechi-aims <kelechi@aims.edu.gh>
2024-01-04 08:11:42 +0100

    task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,31 +1,17 @@
 export default function createIteratorObject(report) {
   const { allEmployees } = report;
-  const departments = Object.keys(allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
 
-  return {
-    next() {
-      if (currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[currentDepartmentIndex];
-        const currentEmployees = allEmployees[currentDepartment];
-
-        if (currentEmployeeIndex < currentEmployees.length) {
-          const result = {
-            value: currentEmployees[currentEmployeeIndex],
-            done: false,
-          };
-          currentEmployeeIndex += 1;
-          return result;
+  const iterator = {
+    * [Symbol.iterator]() {
+      for (const department in allEmployees) {
+        if (Object.prototype.hasOwnProperty.call(allEmployees, department)) {
+          for (const employee of allEmployees[department]) {
+            yield employee;
+          }
         }
-        currentDepartmentIndex += 1;
-        currentEmployeeIndex = 0;
-        return this.next();
       }
-      return { done: true };
-    },
-    [Symbol.iterator]() {
-      return this;
     },
   };
+
+  return iterator;
 }
