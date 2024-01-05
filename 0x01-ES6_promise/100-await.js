b466556fe26f668a682833f680991e8cf9f72cb2
kelechi-aims <kelechi@aims.edu.gh>
2024-01-05 12:39:57 +0100

    task 10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+// 100-await.js
+
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error('Error:', error.message);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
