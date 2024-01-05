d19fd53e8934a3ce0b9eaa9abe20821d8d56d669
kelechi-aims <kelechi@aims.edu.gh>
2024-01-05 09:39:11 +0100

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') throw new Error();
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
