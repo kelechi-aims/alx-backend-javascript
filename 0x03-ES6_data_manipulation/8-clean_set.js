5de9b76fde23438e0838261e033499f16829c6fd
kelechi-aims <kelechi@aims.edu.gh>
2024-01-09 19:38:49 +0100

    task-8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,22 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  /**
+ * const filteredValues = [...set].filter((value) => value.startsWith(startString));
 
-  const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
-  const resultString = cleanedValues.join('-');
-  return resultString;
+ * const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
+ * const resultString = cleanedValues.join('-');
+ * return resultString;
+ */
+  const parts = [];
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
