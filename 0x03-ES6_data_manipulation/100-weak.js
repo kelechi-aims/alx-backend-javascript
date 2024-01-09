9c450cd219744186e4a65c93283d57a2da01ca0b
kelechi-aims <kelechi@aims.edu.gh>
2024-01-09 23:27:01 +0100

    task-11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,19 @@
+// Creates and export a WeakMap to track the number of queries for each endpoint
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+  // Check if the weakMap has an entry for the given endpoint
+  if (!weakMap.has(endpoint)) {
+    // If not, set the initial count to 1
+    weakMap.set(endpoint, 1);
+  } else {
+    // If yes, increment the count
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    // Check if the count is >= 5, throw an error
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
