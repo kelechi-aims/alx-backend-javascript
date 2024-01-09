2eeead94dc10ce2c6c728bc279c00cf6d794b6b4
kelechi-aims <kelechi@aims.edu.gh>
2024-01-09 20:03:55 +0100

    task-9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,12 @@
-export default function () {
+export default function groceriesList() {
+  const groceriesMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1,],
+    ['Banana', 5],
+  ]);
+
+  return groceriesMap;
 
 }
