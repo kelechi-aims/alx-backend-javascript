b2b175a07c69881f4e80a994c54c7ae1ae533a6a
kelechi-aims <kelechi@aims.edu.gh>
2024-01-09 19:53:23 +0100

    task-8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,22 +2,16 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  /**
- * const filteredValues = [...set].filter((value) => value.startsWith(startString));
 
- * const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
- * const resultString = cleanedValues.join('-');
- * return resultString;
- */
-  const parts = [];
-  set.forEach (function(value)) {
+  const resultString = [];
+  set.forEach((value) => {
     if (typeof value === 'string' && value.startsWith(startString)) {
-      const valueSubStr = value.substring(startString.length);
+      const cleanedValues = value.substring(startString.length);
 
-      if (valueSubStr && valueSubStr !== value) {
-        parts.push(valueSubStr);
+      if (cleanedValues && cleanedValues !== value) {
+        resultString.push(cleanedValues);
       }
     }
-  }
-  return parts.join('-');
+  });
+  return resultString.join('-');
 }
