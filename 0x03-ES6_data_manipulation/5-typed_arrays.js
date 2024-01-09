0c90dce9af3479beb30799b20f80e591a5cb4379
kelechi-aims <kelechi@aims.edu.gh>
2024-01-09 17:49:42 +0100

    task-5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const int8Array = new DataView(buffer, 0, length);
 
-  int8Array[position] = value;
+  int8Array.setInt8(position, value);
 
   return int8Array;
 }
