f3166cba341cfac0c6c11432c68b401b6529b9bc
kelechi-aims <kelechi@aims.edu.gh>
2024-02-26 18:46:35 +0100

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,16 +3,15 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 // Listen for input from the standard input stream (STDIN)
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-
     // Display the user's name
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 // Display closing message and ends he program
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
