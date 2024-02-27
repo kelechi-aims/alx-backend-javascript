d5ea05cc18f086d4b423925774ab8afb9473a4e6
kelechi-aims <kelechi@aims.edu.gh>
2024-02-27 12:22:55 +0100

    Create a more complex HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,18 +1,26 @@
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      await countStudents(process.argv[2]);
-      res.end();
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
@@ -21,6 +29,8 @@ const app = http.createServer(async (req, res) => {
   res.end();
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
