c961e09adf6671c339286a590c60899d59265e64
kelechi-aims <kelechi@aims.edu.gh>
2024-02-27 12:52:57 +0100

    Create a more complex HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,35 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+        const fieldCounts = {};
+        const studentData = lines.slice(1);
+        studentData.forEach((line) => {
+          const fields = line.split(',');
+          if (fields.length === 4) {
+            const field = fields[3];
+            fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+          }
+        });
+        let message = `Number of students: ${studentData.length}`;
+
+        Object.entries(fieldCounts).forEach(([field, count]) => {
+          const firstNames = studentData.filter((line) => line.split(',')[3] === field).map((line) => line.split(',')[0]);
+          message = message.concat(`\nNumber of students in ${field}: ${count}. List: ${firstNames.join(', ')}`);
+        });
+        resolve(message);
+      }
+    });
+  });
+}
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
@@ -10,12 +38,13 @@ const app = http.createServer(async (req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      await countStudents(process.argv[2]);
-    //  res.end();
-    } catch (error) {
-      res.end(error.message);
-    }
+    await countStudents(process.argv[2])
+      .then((message) => {
+        res.end(`${message}`);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
   }
   res.statusCode = 404;
   res.end();
