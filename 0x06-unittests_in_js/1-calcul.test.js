8e9d8bb6ac14b4d4eaaae930dce66307c3738e8c
kelechi-aims <kelechi@aims.edu.gh>
2024-02-29 16:22:03 +0100

    readme upload

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -7,10 +7,12 @@ describe('calculateNumber', () => {
       assert.strictEqual(calculateNumber('SUM', -1.7, -2), -4);
       assert.strictEqual(calculateNumber('SUM', -1, -2.7), -4);
     });
+  });
 
   describe('type SUBTRACT', () => {
     it('should return the subtraction of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', -1.7, -2), 0);
       assert.strictEqual(calculateNumber('SUBTRACT', -1, -2.7), 2);
     });
+  }):
 });
