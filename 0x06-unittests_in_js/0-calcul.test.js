26687c2ccb3beecd25d800dc71da063fdca57150
kelechi-aims <kelechi@aims.edu.gh>
2024-02-29 14:39:13 +0100

    readme upload

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,22 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should round up the first argument', function() {
+describe('calculateNumber', () => {
+  it('should round up the first argument', () => {
     assert.strictEqual(calculateNumber(1.1, 3), 4);
     assert.strictEqual(calculateNumber(1.6, 3), 5);
     assert.strictEqual(calculateNumber(1.9, 3), 5);
     assert.strictEqual(calculateNumber(1.0, 3), 4);
   });
 
-  it('should round up the second argument', function() {
+  it('should round up the second argument', () => {
     assert.strictEqual(calculateNumber(1, 2.6), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1, 4.7), 6);
   });
 
-it('should return the sum of rounded numbers', function() {
+it('should return the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
