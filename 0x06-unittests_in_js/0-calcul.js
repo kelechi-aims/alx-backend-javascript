c01b36d812acfef60c417b2c9ed5e3415fc33415
kelechi-aims <kelechi@aims.edu.gh>
2024-02-29 15:13:50 +0100

    readme upload

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,3 @@
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+function calculateNumber(a, b) {return Math.round(a) + Math.round(b)}
 
 module.exports = calculateNumber;
