7ea0fd6c37a98bae61d382fd3ee116e12e8b70c6
kelechi-aims <kelechi@aims.edu.gh>
2024-03-01 05:49:50 +0100

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with type SUM and return 10', () => {
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const consoleSpy = sinon.spy(console, 'log');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+        calculateNumberStub.restore(); // Restore the stub after the test
+        consoleSpy.restore(); // Restore the spy after the test
+    });
+});
